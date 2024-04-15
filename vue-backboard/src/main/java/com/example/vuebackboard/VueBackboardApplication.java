50649679d2665bfcb0c2848b3895deff533291dc
HwanE <qlswp95@gamil.com>
2024-04-15 23:30:11 +0900

    test commit

--- vue-backboard/src/main/java/com/example/vuebackboard/VueBackboardApplication.java
+++ vue-backboard/src/main/java/com/example/vuebackboard/VueBackboardApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VueBackboardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VueBackboardApplication.class, args);
+		SpringApplication.run(VueBackboardApplication.class, args); 
 	}
 
 }
